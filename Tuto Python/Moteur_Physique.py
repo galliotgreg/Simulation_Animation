0947d12cd61bf60693ec6e4fd467baa842751a2a
galliotgreg <galliotgregpro@gmail.com>
2018-01-17 11:59:15 +0100

    fixe blink

--- Tuto Python/Moteur_Physique.py
+++ Tuto Python/Moteur_Physique.py
@@ -45,16 +45,16 @@ class Box:
             self.calcul_position()
 
         pygame.draw.rect(screen, color_red, (self.position[0], self.position[1], self.width_box, self.height_box), thickness)
-        pygame.display.update()
 
 
 class Ground:
     def __init__(self, position):
         self.position = position
+        self.width_ground = width_ground
+        self.height_ground = height_ground
 
     def draw_ground(self):
-        pygame.draw.rect(screen, color_blue, (self.position[0], self.position[1], width_ground, height_ground), thickness)
-        pygame.display.update()
+        pygame.draw.rect(screen, color_blue, (self.position[0], self.position[1], self.width_ground, self.height_ground), thickness)
 
 
 class Scene:
@@ -69,6 +69,7 @@ class Scene:
         self.grounds.append(ground)
 
     def draw_scene(self):
+        screen.fill((0, 0, 0))
         for box in self.boxes:
             box.draw_box()
 
@@ -77,22 +78,20 @@ class Scene:
 
     def calcul_collision(self):
         #collision box vs ground
-        for box in self.boxes :
-            for ground in self.grounds :
-                if(box.position[1] + box.height_box >= ground.position[1]) :
-                    box.isColliding = True
+        for box in self.boxes:
+            for ground in self.grounds:
+                if(box.position[1] + box.height_box >= ground.position[1]):
+                    if(ground.position[0] <= box.position[0] <= ground.position[0]+ ground.width_ground):
+                        box.isColliding = True
 
         #collision box vs box
         for box1 in self.boxes:
             for box2 in self.boxes:
                 if box1 is not box2:
                     if box1.position[1] + box1.height_box >= box2.position[1]:
-                       if box2.position[0] <= box1.position[0] <= (box2.position[0] + box2.width_box):
+                       if box2.position[0] <= box1.position[0] <= (box2.position[0] + box2.width_box) or box2.position[0] <= box1.position[0] + box1.width_box <= (box2.position[0] + box2.width_box):
                            if box2.isColliding:
                                box1.isColliding = True
-                       if box2.position[0] <= box1.position[0] + box1.width_box <= (box2.position[0] + box2.width_box):
-                            if box2.isColliding:
-                                box1.isColliding = True
 
 
 def init():
@@ -107,24 +106,26 @@ def main():
     scene = Scene()
 
     box1 = Box((50, 50), 1000)
-    box2 = Box((100, 50),10000)
+    box2 = Box((90, 50),10000)
     box3 = Box((132, 50),100000)
+    box4 = Box((800, 50), 10000)
 
     ground1 = Ground((0, 760))
 
     scene.add_box(box1)
     scene.add_box(box2)
     scene.add_box(box3)
+    scene.add_box(box4)
 
     scene.add_ground(ground1)
 
     while True:
-        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
         scene.calcul_collision()
         scene.draw_scene()
+        pygame.display.update()
 
 main()
