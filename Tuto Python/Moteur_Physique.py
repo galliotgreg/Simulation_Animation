6e78a3e49179d0f79542676309ce8f36b6e5cf21
galliotgreg <galliotgregpro@gmail.com>
2018-01-17 14:04:41 +0100

    Refacto

--- Tuto Python/Moteur_Physique.py
+++ Tuto Python/Moteur_Physique.py
@@ -19,14 +19,20 @@ delta_t = 0.1
 class Box:
 
     def __init__(self, position, masse):
-        self.width_box = 50
-        self.height_box = 50
+        self.width = 50
+        self.height = 50
+        self.barycentre = (0,0)
 
         self.position = position
         self.vitesse = (0,0)
         self.masse = masse
         self.acceleration = (0,0)
         self.isColliding = False
+
+    def calcul_barycentre(self):
+        barycentre_x = ((self.position[0] + self.width) - self.position[0]) //2
+        barycentre_y = ((self.position[1] + self.height) - self.position[1])//2
+        self.barycentre = (barycentre_x, barycentre_y)
 
     def calcul_position(self):
         self.position = (self.position[0] + self.vitesse[0], self.position[1] + self.vitesse[1])
@@ -37,24 +43,25 @@ class Box:
     def calcul_acceleration(self):
         self.acceleration = (0/self.masse , 9.806/self.masse)
 
-    def draw_box(self):
+    def draw(self):
 
         if not self.isColliding:
             self.calcul_acceleration()
             self.calcul_vitesse()
             self.calcul_position()
+            self.calcul_barycentre()
 
-        pygame.draw.rect(screen, color_red, (self.position[0], self.position[1], self.width_box, self.height_box), thickness)
+        pygame.draw.rect(screen, color_red, (self.position[0], self.position[1], self.width, self.height), thickness)
 
 
 class Ground:
     def __init__(self, position):
         self.position = position
-        self.width_ground = width_ground
-        self.height_ground = height_ground
+        self.width = width_ground
+        self.height = height_ground
 
     def draw_ground(self):
-        pygame.draw.rect(screen, color_blue, (self.position[0], self.position[1], self.width_ground, self.height_ground), thickness)
+        pygame.draw.rect(screen, color_blue, (self.position[0], self.position[1], self.width, self.height), thickness)
 
 
 class Scene:
@@ -71,7 +78,7 @@ class Scene:
     def draw_scene(self):
         screen.fill((0, 0, 0))
         for box in self.boxes:
-            box.draw_box()
+            box.draw()
 
         for ground in self.grounds:
             ground.draw_ground()
@@ -80,21 +87,23 @@ class Scene:
         #collision box vs ground
         for box in self.boxes:
             for ground in self.grounds:
-                if box.position[1] + box.height_box >= ground.position[1]:
-                    if ground.position[0] <= box.position[0] <= ground.position[0]+ ground.width_ground:
+                if box.position[1] + box.height >= ground.position[1]:
+                    if ground.position[0] <= box.position[0] <= ground.position[0]+ ground.width:
                         box.isColliding = True
-                        box.position = (box.position[0],ground.position[1] - box.height_box)
+                        box.position = (box.position[0],ground.position[1] - box.height)
 
         #collision box vs box
         for box1 in self.boxes:
             for box2 in self.boxes:
                 if box1 is not box2:
-                    if box1.position[1] + box1.height_box >= box2.position[1]:
-                        if box2.position[0] <= box1.position[0] <= (box2.position[0] + box2.width_box) or box2.position[0] <= box1.position[0] + box1.width_box <= (box2.position[0] + box2.width_box):
+                    if box1.position[1] + box1.height >= box2.position[1]:
+                        if box2.position[0] <= box1.position[0] <= (box2.position[0] + box2.width) or box2.position[0] <= box1.position[0] + box1.width <= (box2.position[0] + box2.width):
                             if box2.isColliding:
                                 if not box1.isColliding:
                                     box1.isColliding = True
-                                    box1.position = (box1.position[0], box2.position[1] - box1.height_box)
+                                    box1.position = (box1.position[0], box2.position[1] - box1.height)
+
+
 
 
 def init():
