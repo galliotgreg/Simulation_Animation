a88cf3cd3bc4ccd420840b00df7352ef4a57efb6
galliotgreg <galliotgregpro@gmail.com>
2018-01-23 12:02:33 +0100

    Add invicible walls

--- Tuto Python/Fluide_Simulation.py
+++ Tuto Python/Fluide_Simulation.py
@@ -20,24 +20,39 @@ class Particle:
         self.vitesse = (self.vitesse[0] + self.acceleration[0], self.vitesse[1] + self.acceleration[1])
 
     def calcul_position(self):
-        x = min(self.position[0] + self.vitesse[0], 1024)
-        y = min(self.position[1] + self.vitesse[1], 750)
+        x = self.position[0] + self.vitesse[0]
+        y = self.position[1] + self.vitesse[1]
         self.position = (x, y)
 
     def calcul_acceleration(self):
         self.acceleration = (self.masse * g[0], self.masse * g[1])
 
     def draw(self):
-        self.calcul_acceleration()
-        self.calcul_vitesse()
+        if self.position[0] > 1024:
+            self.vitesse = (-self.vitesse[0], self.vitesse[1])
+        if self.position[0] < 0:
+            self.vitesse = (-self.vitesse[0], self.vitesse[1])
+
+        if self.position[1] > 768:
+            self.vitesse = (self.vitesse[0], -self.vitesse[1])
+
+        else:
+            self.calcul_acceleration()
+            self.calcul_vitesse()
+
         self.calcul_position()
-        pygame.draw.circle(screen, self.color, self.position, 3, 1)
+
+        pygame.draw.circle(screen, self.color, self.position, 5, 3)
 
 
 class Scene:
     def __init__(self, nb_particles):
         self.particles = []
         for i in range(0, nb_particles):
+            rand_v_x = randint(0,100)
+            rand_v_y = randint(0,100)
+            rand_v_x_signe = randint(-1,1)
+            rand_v_y_signe = randint(-1, 1)
             rand_r = randint(0, 255)
             rand_g = randint(0, 255)
             rand_b = randint(0, 255)
@@ -46,6 +61,7 @@ class Scene:
             rand_masse = randint(1, 2)
             p = Particle((rand_x, rand_y), rand_masse)
             p.color = (rand_r, rand_g, rand_b)
+            p.vitesse = (rand_v_x_signe * rand_v_x, rand_v_y_signe * rand_v_y)
             self.particles.append(p)
 
     def draw(self):
@@ -62,7 +78,7 @@ def init():
 def main():
 
     init()
-    scene = Scene(10)
+    scene = Scene(500)
 
     while True:
         pygame.time.delay(50)
